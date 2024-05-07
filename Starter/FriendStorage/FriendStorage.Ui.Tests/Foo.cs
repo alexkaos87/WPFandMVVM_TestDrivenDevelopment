0717ffd8aacbf6f050987ca77ddc448ebd780d68
alexkaos <alessiosalvatore87@gmail.com>
2024-05-07 10:01:39 +0200

    create empty test project

---
+++ Starter/FriendStorage/FriendStorage.Ui.Tests/Foo.cs
@@ -0,0 +1,20 @@
+﻿using FluentAssertions;
+using Xunit;
+
+namespace FriendStorage.Ui.Tests
+{
+    public class Foo
+    {
+        [Fact]
+        public void Test1()
+        {
+            var var1 = true;
+            var1.Should().BeTrue();
+        }
+
+        [Theory]
+        [InlineData(5, 2, 3)]
+        [InlineData(10, 3, 7)]
+        public void Test2(in int expected, in int first, in int second) => (first + second).Should().Be(expected);
+    }
+}
